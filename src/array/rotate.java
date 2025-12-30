6f4ba4e81b33892d2525c2ec9ccd3592e67375f2
dksharma15 <dipanshu.k.sharma15@gmail.com>
2025-12-30 23:19:14 +0530

    updated code to handle case when k is greater than length of array

--- src/array/rotate.java
+++ src/array/rotate.java
@@ -4,6 +4,7 @@ public class rotate {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8};
         int k = 3;
+        k = k % arr.length; // if k is gretaer than length this will handle
         int[] brr = new int[arr.length];
 
         for(int i=0; i<arr.length; i++){
