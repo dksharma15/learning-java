c3aad5b344efc41075ac19231d6aac84f81da4fb
dksharma15 <dipanshu.k.sharma15@gmail.com>
2026-01-08 22:49:37 +0530

    Added program to print transpose of a matrix

---
+++ src/TwoDimensionalArray/transpose.java
@@ -0,0 +1,33 @@
+package TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class transpose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] brr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                brr[i][j] = arr[j][i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(brr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        
+
+        sc.close();
+    }
+}
