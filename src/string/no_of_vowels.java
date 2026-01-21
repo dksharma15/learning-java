25d6c36d76b7dab12c5e4f026883bf3c4da8035f
dksharma15 <dipanshu.k.sharma15@gmail.com>
2026-01-21 22:45:35 +0530

    Added program to count number of vowels in string

---
+++ src/string/no_of_vowels.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class no_of_vowels {
+    public static boolean vowel(char ch){
+        if(ch=='a' || ch=='A') return true;
+        else if(ch=='e' || ch=='E') return true;
+        else if(ch=='i' || ch=='I') return true;
+        else if(ch=='o' || ch=='O') return true;
+        else if(ch=='u' || ch=='U') return true;
+        else return false;
+    }
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String str = sc.nextLine();
+       int count = 0;
+       
+       for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(vowel(ch)==true) count++;
+       }
+       System.out.println(count);
+       sc.close();
+    }
+}
