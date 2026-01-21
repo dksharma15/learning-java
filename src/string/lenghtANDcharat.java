f6d34688994a2b09c57a658bfc7cfc7466b5bbf8
dksharma15 <dipanshu.k.sharma15@gmail.com>
2026-01-21 22:12:15 +0530

    Added program to use charAt and length()

---
+++ src/string/lenghtANDcharat.java
@@ -0,0 +1,10 @@
+package string;
+
+public class lenghtANDcharat {
+    public static void main(String[] args) {
+        String str = "dipanshu kumar sharma";
+        System.out.println(str.charAt(1));
+
+        System.out.println(str.length());
+    }
+}
